5bd42cac4f236f5d247da9a7b2df9ac5f032ec18
mbenji99 <mbenji99@gmail.com>
2023-04-28 00:36:07 -0500

    updated variable names in backends

--- app.py
+++ app.py
@@ -48,130 +48,129 @@ def getUsers():
 
 @app.route('/courses', methods=['POST'])
 def createCourse():
+    courseCode = "C" + str(dbm.get_next_course_id())
+    course_name = request.form.get('course_name')
+    start_date = request.form.get('start_date')
+    credits = request.form.get('credits')
     admin_user_type = "admin"
     userID = request.form.get('userID')
     passW = request.form.get('passW')
-    course_name = request.form.get('courseName')
-    start_date = request.form.get('startDate')
 
     # check if the user is an admin
     user = dbm.get_user_by_id(userID)
     if user and user['passW'] == passW and user['userType'] == admin_user_type:
-        # generate a new course ID
-        c_id = "C" + str(dbm.get_next_course_id())
-
         # insert the new course into the database
-        dbm.create_course({"c_id": c_id, "course_name": course_name, "start_date": start_date})
+        dbm.create_course({"courseCode": courseCode, "course_name": course_name, "start_date": start_date, "credits": credits})
 
-        return make_response({"Status": "Course created successfully.", "c_id": c_id}, 200)
+        return make_response({"Status": "Course created successfully.", "courseCode": courseCode}, 200)
     else:
         return make_response({"Status": "You do not have permission to create a course."}, 401)
 
-
 @app.route('/courses', methods=['GET'])
-def getCourses():
-    course_id = request.args.get('courseID')
-    student_id = request.args.get('studentID')
-    lecturer_id = request.args.get('lecturerID')
+def get_courses():
+    course_code = request.args.get('courseCode')
+    stud_id = request.args.get('stud_id')
+    lect_id = request.args.get('lect_id')
 
-    if not any([course_id, student_id, lecturer_id]):
+    if not any([course_code, stud_id, lect_id]):
         return make_response({"Status": "Please provide a query parameter to retrieve courses."}, 400)
 
-    if course_id:
+    if course_code:
         # retrieve a specific course
-        course = dbm.get_course_by_id(course_id)
+        course = dbm.get_course_by_code(course_code)
         if course:
             return make_response(course, 200)
         else:
             return make_response({"Status": "Course not found."}, 404)
-    elif student_id:
+    elif stud_id:
         # retrieve courses for a specific student
-        courses = dbm.get_courses_by_student_id(student_id)
+        courses = dbm.get_courses_by_student_id(stud_id)
         return make_response(courses, 200)
-    elif lecturer_id:
+    elif lect_id:
         # retrieve courses taught by a specific lecturer
-        courses = dbm.get_courses_by_lecturer_id(lecturer_id)
+        courses = dbm.get_courses_by_lecturer_id(lect_id)
         return make_response(courses, 200)
+
 
 @app.route('/register_course', methods=['POST'])
-def registerCourse():
-    course_id = request.form.get('courseID')
-    student_id = request.form.get('studentID')
-    lecturer_id = request.form.get('lecturerID')
+def register_course():
+    course_code = request.form.get('courseCode')
+    student_id = request.form.get('stud_id')
+    lecturer_id = request.form.get('lect_id')
 
-    if not course_id:
-        return make_response({"Status": "Please provide a course ID."}, 400)
+    if not course_code:
+        return make_response({"Status": "Please provide a course code."}, 400)
 
     # check if the course exists
-    course = dbm.get_course_by_id(course_id)
+    course = dbm.get_course_by_code(course_code)
     if not course:
         return make_response({"Status": "Course not found."}, 404)
 
     # check if a lecturer is already assigned to the course
-    if course['lecturerID']:
+    if course['lect_id']:
         return make_response({"Status": "A lecturer is already assigned to this course."}, 400)
 
     # if the lecturer ID is provided, check if it is valid
     if lecturer_id:
-        lecturer = dbm.get_user_by_id(lecturer_id)
-        if not lecturer or lecturer['userType'] != 'lecturer':
+        lecturer = dbm.get_lecturer_by_id(lecturer_id)
+        if not lecturer:
             return make_response({"Status": "Invalid lecturer ID."}, 400)
-        course['lecturerID'] = lecturer_id
+        course['lect_id'] = lecturer_id
 
     # if the student ID is provided, register the student for the course
     if student_id:
-        student = dbm.get_user_by_id(student_id)
-        if not student or student['userType'] != 'student':
+        student = dbm.get_student_by_id(student_id)
+        if not student:
             return make_response({"Status": "Invalid student ID."}, 400)
         # check if the student is already registered for the course
-        if dbm.is_registered(course_id, student_id):
+        if dbm.is_enrolled(course_code, student_id):
             return make_response({"Status": "Student already registered for the course."}, 400)
-        dbm.register_student_for_course(course_id, student_id)
+        dbm.enroll_student_in_course(course_code, student_id)
 
     # update the course in the database
     dbm.update_course(course)
 
     return make_response({"Status": "Registration successful."}, 200)
 
-@app.route('/courses/<course_id>/events', methods=['GET'])
-def getCalendarEvents(course_id):
-    events = dbm.get_events_by_course_id(course_id)
+@app.route('/courses/<courseCode>/events', methods=['GET'])
+def getCalendarEvents(courseCode):
+    events = dbm.get_events_by_course_code(courseCode)
     if events:
         return make_response(events, 200)
     else:
         return make_response({"Status": "No calendar events found for this course."}, 404)
 
-@app.route('/students/<student_id>/events', methods=['GET'])
-def getStudentCalendarEvents(student_id):
+@app.route('/students/<stud_id>/events', methods=['GET'])
+def getStudentCalendarEvents(stud_id):
     date = request.args.get('date')
     if not date:
         return make_response({"Status": "Please provide a date query parameter."}, 400)
 
-    events = dbm.get_events_by_student_id_and_date(student_id, date)
+    events = dbm.get_events_by_student_id_and_date(stud_id, date)
     if events:
         return make_response(events, 200)
     else:
         return make_response({"Status": "No calendar events found for this student on this date."}, 404)
 
-@app.route('/courses/<course_id>/events', methods=['POST'])
-def createCalendarEvent(course_id):
+@app.route('/courses/<courseCode>/events', methods=['POST'])
+def createCalendarEvent(courseCode):
     admin_user_type = "admin"
-    userID = request.form.get('userID')
+    u_id = request.form.get('u_id')
     passW = request.form.get('passW')
-    event_name = request.form.get('eventName')
+    event_title = request.form.get('eventTitle')
     event_date = request.form.get('eventDate')
-    event_desc = request.form.get('eventDescription')
+    event_details = request.form.get('eventDetails')
 
     # check if the user is an admin
-    user = dbm.get_user_by_id(userID)
+    user = dbm.get_user_by_id(u_id)
     if user and user['passW'] == passW and user['userType'] == admin_user_type:
         # generate a new event ID
-        e_id = "E" + str(dbm.get_next_event_id())
+        event_id = "E" + str(dbm.get_next_event_id())
 
         # insert the new event into the database
-        dbm.create_event({"e_id": e_id, "course_id": course_id, "event_name": event_name, "event_date": event_date, "event_desc": event_desc})
+        dbm.create_event({"event_id": event_id, "courseCode": courseCode, "event_title": event_title, "event_date": event_date, "event_details": event_details})
 
-        return make_response({"Status": "Event created successfully.", "e_id": e_id}, 200)
+        return make_response({"Status": "Event created successfully.", "event_id": event_id}, 200)
     else:
         return make_response({"Status": "You do not have permission to create an event."}, 401)
 
@@ -196,6 +195,7 @@ def getReports():
         return make_response(students, 200)
     else:
         return make_response({"Status": "Invalid report type."}, 400)
+        
 if __name__ == "__main__":
     dbm.genCreateSQL()
     dbm.genInsertSQL()
